c3dc6bb804cdb596c0beefeb6bc725efc4e6ffb9
Keanu-Rico Brüggemann <keanu-rico.brueggemann@web.de>
2024-04-05 19:45:07 +0200

    JSON file reads succesfully into a data object and gets sent to the index.ejs correctly. Next step frontend work. In the future using an actual database would be nice.

--- index.js
+++ index.js
@@ -5,48 +5,68 @@ import * as fs from 'fs';
 const app = express();
 const port = 3000;
 
-var data = [{
-  text: 'test',
-  name: 'testt',
-  date: 'testttt',
-}];
+
 
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  console.log(data[0]);
-  res.render("index.ejs", data[0], data[1]);
-});
-
-app.post("/submit", (req, res) => {
-  data.push({
-    text: req.body.thought,
-    name: req.body.name,
-    date: new Date(),
-  }) ;
-
-  fs.writeFile('data/URL.json', JSON.stringify(data), err => {
-    if (err) {
-        console.error(err);
-      } else {
-        console.log('Donesies');
-      }
-  res.render("index.ejs", data[0, 1]);
-
   fs.readFile("data/URL.json", (err, data) => {
     if (err) {
       console.log(err);
     }
-    var json = JSON.parse(data);
-    console.log(json);
-    console.log(json[1].text);
-  })
+    if (data) {
+      var json = JSON.parse(data);
+      res.render("index.ejs", {input: json});
+    }
+    else {
+      res.render("index.ejs");
+    }
   });
 
+});
 
+app.post("/submit", (req, res) => {
+  if (fs.existsSync("data/URL.json")) {
+    fs.readFile("data/URL.json", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      
+      var json = JSON.parse(data);
 
+      json.push({
+        text: req.body.thought,
+        name: req.body.name,
+        date: new Date()
+      });
+      fs.writeFile('data/URL.json', JSON.stringify(json), err => {
+        if (err) {
+            console.error(err);
+          } else {
+            console.log('Donesies');
+          }
+      res.render("index.ejs", {input: json});
+      });
+    }); 
+   } else {
+    var json = [{
+      text: req.body.thought,
+      name: req.body.name,
+      date: new Date()
+    }];
+    fs.writeFile('data/URL.json', JSON.stringify(json), err => {
+      if (err) {
+          console.error(err);
+        } else {
+          console.log('Donesies');
+        }
+    res.render("index.ejs", {input: json});
+    });
+
+  }
+  
 });
 
 app.post("/delete", (req, res) => {
@@ -56,12 +76,17 @@ app.post("/delete", (req, res) => {
       } else {
         console.log('Donesies');
       }
-});
+    res.render("index.ejs");
+  });
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+
+
 
 
 
